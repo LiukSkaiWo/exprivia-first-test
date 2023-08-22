0ca90178b58eeb04379e646eefe0674adaf6d0b8
LiukSkaiWo <albergo.vargroup@gmail.com>
2023-08-22 14:24:16 +0200

    Uploaded blank project to the repository

---
+++ src/main/java/it/xa/first/test/RESTful/project/ResTfulProjectApplication.java
@@ -0,0 +1,13 @@
+package it.xa.first.test.RESTful.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/xa/first/test/RESTful/project/ResTfulProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.xa.first.test.RESTful.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
